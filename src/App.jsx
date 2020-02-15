65d66f8059093854fe71367f405375a8eb1f969e
Yaxel Perez <yaxelp@gmail.com>
2020-02-15 12:12:41 -0800

    react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (<p>hello world</p>)
+  }
+}
+
+export default App;
